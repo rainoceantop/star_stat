fb0ce73dfc7369a8a8e3b619cea5df866690e0d2
rainoceantop <rainoceantop@outlook.com>
2019-12-22 20:28:28 +0800

    fs

--- backend/routes/article.js
+++ backend/routes/article.js
@@ -5,7 +5,7 @@ const articleMiddleware = require('../middleware/article')
 
 /* GET articles listing. */
 router.post('/create', articleMiddleware.create, articleController.create)
-router.post('/getArticles/:uid', articleMiddleware.getSelfArticles, articleController.getSelfArticles)
+router.post('/getArticles/:uid', articleMiddleware.getArticles, articleController.getArticles)
 router.get('/getArticle/:id', articleMiddleware.getArticle, articleController.getArticle)
 router.post('/delete', articleMiddleware.delete, articleController.delete)
 router.post('/update', articleMiddleware.update, articleController.update)
